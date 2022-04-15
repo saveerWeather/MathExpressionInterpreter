d7df908ae2b629345f761c7c7f4450d54e1dca24
saveerWeather <saveerjain105@gmail.com>
2022-04-14 20:01:15 -0400

    heroku modifications

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 
 var app = express(); 
-const port = 3000; 
+const port = process.env.PORT ||3000; 
 
 app.use(express.static(path.join(__dirname,'./public')));
 
 app.listen(port,()=>{
-    console.log("Server running on port "+ port);
+    console.log("Server running on port "+port);
 })
 
